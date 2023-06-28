657ec880594a2d5f920b6dd97a14da02d75f903a
Fithamlak <justiceofgod01@gmail.com>
2023-06-28 19:47:58 +0300

    implement main component to the list of automobiles

---
+++ src/components/Body.js
@@ -0,0 +1,20 @@
+/* eslint-disable jsx-quotes */
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Body = () => (
+  <header className="header">
+    <span className="header-title">Book a tcket with your prefer automobile</span>
+    <div className="header-automobile-btn-space">
+      <span className="header-automobile-btn">
+        <Link to="/signin" className="link-sign">Sign In</Link>
+      </span>
+      <span className="space">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+      <span className="header-automobile-btn-signup">
+        <Link to="/signup" className="link-sign">Sign Up</Link>
+      </span>
+    </div>
+  </header>
+);
+
+export default Body;
